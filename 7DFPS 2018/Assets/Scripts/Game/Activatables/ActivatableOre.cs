1506a23004a4741ec3cfd6c2cbe9f49bb46b0165
Zaexides <JeroenEndenburg@hotmail.com>
2018-10-25 23:22:29 +0200

    SFX Implementations (Falling & Tagging)

--- 7DFPS 2018/Assets/Scripts/Game/Activatables/ActivatableOre.cs
+++ 7DFPS 2018/Assets/Scripts/Game/Activatables/ActivatableOre.cs
@@ -6,6 +6,7 @@ public class ActivatableOre : Activatable, IWorldGenObject
 {
     public string activateText = "Tag Cobalt Ore";
     public GameObject tagObjectPrefab;
+    public AudioSource tagSfx;
     private bool tagged;
 
     public int ore = 5;
@@ -41,6 +42,7 @@ public class ActivatableOre : Activatable, IWorldGenObject
     {
         Tag();
         BaseController.Main.taggedOre += ore;
+        tagSfx.Play();
     }
 
     private void Tag()
