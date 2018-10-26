201192aa07b1a59a602e8142d010911a7fd92bba
Zaexides <JeroenEndenburg@hotmail.com>
2018-10-26 17:53:17 +0200

    Upgrade Terminal

---
+++ 7DFPS 2018/Assets/Scripts/Game/Activatables/ActivatableUpgradeTerminal.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatableUpgradeTerminal : Activatable
+{
+    public GameObject terminalCanvas;
+
+    public override string ActivateInfo
+    {
+        get
+        {
+            return "Access Upgrades";
+        }
+    }
+
+    public override void Activate(EntityPlayer player)
+    {
+        terminalCanvas.SetActive(true);
+        GameManager.Main.OpenMenu(terminalCanvas.GetComponent<ICloseableMenu>());
+    }
+}
---
+++ 7DFPS 2018/Assets/Scripts/Game/UI/UpgradeMenu.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeMenu : MonoBehaviour, ICloseableMenu
+{
+    public Button createButton;
+    public EntityPlayer entityPlayer;
+
+    public Button sonarUpgrade;
+    public int sonarCost;
+
+    private Upgrade targetUpgrade;
+
+    public Color selectColor, unselectColor;
+
+    private void OnEnable()
+    {
+        createButton.interactable = false;
+        sonarUpgrade.interactable = !entityPlayer.hasSonar && BaseController.Main.ore >= sonarCost;
+    }
+
+    public void TargetSonar() => SetUpgradeTarget(Upgrade.Sonar);
+
+    public void SetUpgradeTarget(Upgrade upgrade)
+    {
+        targetUpgrade = upgrade;
+        sonarUpgrade.GetComponent<Image>().color = upgrade == Upgrade.Sonar ? selectColor : unselectColor;
+
+        createButton.interactable = upgrade != Upgrade.None;
+    }
+
+    public void CreateButton()
+    {
+        switch(targetUpgrade)
+        {
+            case Upgrade.Sonar:
+                AddSonar();
+                break;
+        }
+
+        SetUpgradeTarget(Upgrade.None);
+    }
+
+    public void AddSonar()
+    {
+        entityPlayer.hasSonar = true;
+        BaseController.Main.ore -= sonarCost;
+        sonarUpgrade.interactable = false;
+        targetUpgrade = Upgrade.None;
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+        GameManager.Main.CloseMenu();
+    }
+
+    public enum Upgrade
+    {
+        None,
+        Sonar,
+    }
+}
