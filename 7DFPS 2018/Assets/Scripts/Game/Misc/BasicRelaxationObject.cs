652f3758c0c1cec6f13ba84571869b120cd8b4bb
Zaexides <JeroenEndenburg@hotmail.com>
2018-10-26 22:28:10 +0200

    Decoratives / anxiety relievers

---
+++ 7DFPS 2018/Assets/Scripts/Game/Misc/BasicRelaxationObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicRelaxationObject : MonoBehaviour, IRelaxationObject
+{
+    public float anxietyDecreaseAmount;
+    public float AnxietyDecreaseAmount
+    {
+        get
+        {
+            return anxietyDecreaseAmount;
+        }
+    }
+}
